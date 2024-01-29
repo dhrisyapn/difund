41b0450aae76065df34572af5a42ce57b5ecbfd2
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-01-29 14:48:24 +0530

    payment page completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:difund/donate.dart';
 import 'package:difund/firebase_options.dart';
 import 'package:difund/forgot.dart';
 import 'package:difund/login.dart';
+import 'package:difund/payment.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       
       ),
     debugShowCheckedModeBanner: false,
-    home: LoginPage(),
+    home: PaymentPage(),
     );
   }
 }
